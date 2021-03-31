0a9718c8760599b71d81d678cc72b3dca38140bb
hafeezullahkakar <hafeezullahkakar@gmail.com>
2021-04-01 01:22:26 +0500

    Did some style for responsivenes

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-function Refresh(){  
-var x = random(8);  
-return x;
-
+function Refresh() {
+  var x = random(8);
+  return x;
 }
-function random(size){
-    var chars = '!@#$%^&*()_+,./?:;[]{}\|~`abcdefghijklmnopqrstuvwxyz1234567890';
-    var len = chars.length;
-    let randChar= '';
-    for(i =0; i<=size; i++){
-    var randNo = Math.random()*len+1;
-     randChar += chars.charAt(randNo);      
-    }   
-     return randChar;
-    
+function random(size) {
+  var chars = "!@#$%^&*()_+,./?:;[]{}|~`abcdefghijklmnopqrstuvwxyz1234567890";
+  var len = chars.length;
+  let randChar = "";
+  for (i = 0; i <= size; i++) {
+    var randNo = Math.random() * len + 1;
+    randChar += chars.charAt(randNo);
+  }
+  return randChar;
 }
-
-
-
-
